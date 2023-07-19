6ef5aba34f88716b504042d3c8aaf89376cf5c5f
jeremy-sinclaire <it@propulse-lab.com>
2023-07-19 10:48:26 +0200

    Add this part: Once events have stopped coming through, print the highest token-value accounts by                parent_program_subtype (taking into account write version), and gracefully shut-down the                system.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import { sleep } from './helpers.js'
 import Bull from 'bull'
-
+import { getMongoClient, connectToDatabase, sleep } from './helpers.js'
 // Real-time system that processes a live stream of account updates
 
 async function readAccountUpdatesStream () {
@@ -15,9 +14,13 @@ async function readAccountUpdatesStream () {
 
     // THERE IS AN INCOMING ACCOUNT
     if (waitingJobs.length > 0) {
+
+      // get the last job
       const job = waitingJobs[waitingJobs.length - 1]
 
       await job.remove()
+      if (job.data.version !== undefined) {
+        console.log('CORRUPTED DATA IN JOB - DISCARDING DATA', job.data.id)
 
       // get running processes
       const accountProcessJobs = await accountProcessQueue.getJobs(['delayed'])
@@ -49,7 +52,13 @@ async function readAccountUpdatesStream () {
             console.log('CANCEL THE OLD JOB + SCHEDULE A NEW ONE - VERSION', processingJob.data.version, 'ID', processingJob.data.id)
             // cancel the old job
             const toRemove = await accountProcessQueue.getJob(processingJob.id)
-            await toRemove.remove()
+
+            try {
+             await toRemove.remove()
+            } catch (error) {
+            console.log('ERROR REMOVING JOB', error)
+            }
+
 
             // add the new job
             await accountProcessQueue.add(job.data, { delay: job.data.callbackTimeMs })
@@ -60,17 +69,68 @@ async function readAccountUpdatesStream () {
         }
       }
     }
+  }
     tick++
-    await sleep(1000)
-  } while (tick !== 1000000)
+    await sleep(100)
+  } while (tick < 1200)
 
   return null
+}
+
+async function printData () {
+  console.log('PRINT DATA STARTED')
+  try {
+    await connectToDatabase()
+    const client = await getMongoClient()
+    const myColl = client.collection('solanaAccounts')
+    const parentProgramSubTypes = await myColl.distinct('parentProgramSubType')
+    const latestVersion = []
+    for (const parentProgramSubType of parentProgramSubTypes) {
+      const accountsOfOneParentProgramSubType = await myColl.find({ parentProgramSubType }).toArray()
+      const distinctAccounts = [...new Set(accountsOfOneParentProgramSubType.map(item => item.id))]
+      for (const id of distinctAccounts) {
+        const account = accountsOfOneParentProgramSubType.filter(item => item.id === id).sort((a, b) => a.version - b.version)
+
+        const lastVersion = account[account.length - 1]
+        latestVersion.push(lastVersion)
+      }
+    }
+
+    parentProgramSubTypes.forEach( (item) => {
+      const version = latestVersion
+        .filter(item2 => item2.parentProgramSubType === item)
+        .sort((a, b) => a.tokens - b.tokens)
+        if (version.length > 1) {
+          const highest = version[version.length - 1]
+          console.log('highestToken accounts by parentProgramSubType',
+            'parentProgramSubType', highest.parentProgramSubType,
+            'token', highest.tokens,
+            'id', highest.id,
+            'version', highest.version)
+        }
+        if (version.length === 1){
+          console.log('highestToken accounts by parentProgramSubType',
+            'parentProgramSubType', version[0].parentProgramSubType,
+            'token', version[0].tokens,
+            'id', version[0].id,
+            'version', version[0].version)
+        }
+    })
+  } catch (error) {
+    console.error('main process::An error occurred:', error)
+    return error
+  } finally {
+    const db = await getMongoClient()
+    await db.client.close()
+  }
 }
 
 // Start the program
 (async () => {
   try {
     const result = await readAccountUpdatesStream()
+    await sleep(30000)
+    await printData()
     console.log('Main process::result', result)
     process.exit(0)
   } catch (error) {
